523344fc068c980f13c84861039a2cb22b9d5eb9
xbkv <one.two.o.two@icloud.com>
2025-07-01 09:28:33 +0900

    Use env variable for MongoDB collection name

---
+++ source/models/Bot.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Schema } from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI);
+
+interface IBot extends Document {
+    user_id: string;
+    email: string;
+    password: string;
+    access_token: string;
+    uuid: string;
+}
+
+const userSchema = new Schema({
+    user_id: { type: String, required: true },
+    email: { type: String, required: true },
+    password: {  type: String, required: true },
+    access_token: { type: String, required: true, Encrypt: true },
+    uuid: { type: String, required: true },
+});
+
+const BotModel = mongoose.model<IBot>(process.env.USER_DB_NAME, userSchema);
+
+export { IBot, BotModel };  1
